c939517ef5d93a0562d5735a4a7c4b5816940f68
dilanunver <dunver33@gmail.com>
2021-12-08 19:21:10 +0100

    show error message

--- src/components/SingleInput.js
+++ src/components/SingleInput.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SingleInput = ({ value, onChange, className = 'single-input', label, placeholder, required }) => {
+  const [showErrorMessage, setShowErrorMessage] = useState(false)
 
-  const isEmptyValue = required && value === ''
+  const handleOnChange = (e) => {
+    setShowErrorMessage(true)
+    console.log('çağrıldı')
+    onChange(e)
+  }
+
+  const isEmptyValue = required && value === '' && showErrorMessage
+  console.log(isEmptyValue)
 
   return (
 
     <div className={className}>
       <label>{label}</label>
-      <input value={value} placeholder={placeholder} className={` ${isEmptyValue && 'empty'}`} onChange={onChange}></input>
+      <input value={value} placeholder={placeholder} className={` ${isEmptyValue && 'empty'}`} onChange={handleOnChange}></input>
       {isEmptyValue && <div className='error-message'>Required field missing</div>}
     </div>
 
