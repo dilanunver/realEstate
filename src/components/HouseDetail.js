1539ad0650edded935ca934b98ddd67fdbf5c4e6
dilanunver <dunver33@gmail.com>
2021-12-13 12:32:24 +0100

    house details with paragraph & delete/edit object

--- src/components/HouseDetail.js
+++ src/components/HouseDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import housepic from '../pictures/housepic.png'
 import location from '../pictures/location.png'
@@ -9,19 +9,37 @@ import bedPc from '../pictures/ic_bed.png'
 import bathPc from '../pictures/ic_bath.png'
 import garagePc from '../pictures/ic_garage.png'
 import { LoremIpsum } from 'react-lorem-ipsum';
+import deleteObj from '../pictures/delete.png'
+import editObj from '../pictures/edit.png'
 
 
 const HouseDetail = () => {
+
+  const [isHovering, setIsHovering] = useState(false)
+
 
   return (
     <div className='postted-house'>
       <Link className='back' to='/house'>Back to overview</Link>
       <div className="house-detail">
-        <div className="bc-white">
+        <div className="bc-white"
+
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
+        >
           <img src={housepic} className='postted' alt='postted'></img>
 
-          <h2 className="postted-header">Stokvisstraat 132</h2>
+          <div className="holding-items">
+            <h2 className="postted-header">Stokvisstraat 132</h2>
+            {isHovering &&
+              <span className="hovering-items">
+                <img className="editObj" src={editObj} alt="editObj" />
+                <img src={deleteObj} alt="editObj" />
+              </span>
+            }
+          </div>
           <div className='postted-city'>
+
             <img className="location" src={location} alt="location"></img> 1011 AA Amsterdam
           </div>
           <div className='postted-price'>
@@ -31,9 +49,9 @@ const HouseDetail = () => {
           </div>
 
           <div className='postted-insideHouse'>
-            <img className="bed-pc" src={bedPc} alt="bed-pc"></img> 1
-            <img className="bath-pc" src={bathPc} alt="bath-pc"></img> 1
-            <img className="garage-pc" src={garagePc} alt="garage-pc"></img> Yes
+            <img className="bed-pc" src={bedPc} alt="bed-pc"></img><span>1</span>
+            <img className="bath-pc" src={bathPc} alt="bath-pc"></img><span>1</span>
+            <img className="garage-pc" src={garagePc} alt="garage-pc"></img><span>Yes</span>
           </div>
           <div className="loremipsum">
             <LoremIpsum p={1} />
